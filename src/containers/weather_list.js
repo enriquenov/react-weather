c9a47d9328167681d943aaf83eea6aa7c13a9e81
enriquenov <enriquenovoa@CHRISTOPHERs-iMac.local>
2017-11-22 00:00:12 -0800

    third commit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,35 +5,29 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => ((temp - 273) * 9/5) + 32 );
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
-        <td>
-          <Chart data={pressures} color="green" />
-        </td>
-        <td>
-          <Chart data={humidities} color="blue" />
-        </td>
+        <th scope="row">{name}</th>
+        <td><Chart data={temps} color="orange" units="&#176;F" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
       </tr>
-    )
+    );
   }
 
   render() {
     return (
-      <table className="table table-hover">
+      <table className="table table-bordered table-hover">
         <thead>
           <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th scope="col">City</th>
+            <th scope="col">Temperature (&#176;F)</th>
+            <th scope="col">Pressure (hPa) </th>
+            <th scope="col">Humidity (%)   </th>
           </tr>
         </thead>
         <tbody>
