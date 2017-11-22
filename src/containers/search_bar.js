78392c03d65bd8811979cc049ef76f941aea79c6
enriquenov <enriquenovoa@CHRISTOPHERs-iMac.local>
2017-11-21 23:17:27 -0800

    second commit

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -33,7 +33,8 @@ class SearchBar extends Component {
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
-          onChange={this.onInputChange} />
+          onChange={this.onInputChange}
+          autofocus/>
 
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
